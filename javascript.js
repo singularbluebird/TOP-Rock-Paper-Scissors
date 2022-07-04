646aa9c2e5830ba1bde355233a53d28e209c53b6
Samuel <samysarria.ss@gmail.com>
2022-07-03 22:04:02 -0300

    Changes implementation of computerPlay
    Previously used switch statements, now it is just choosing an
    element in an array.

--- javascript.js
+++ javascript.js
@@ -12,21 +12,7 @@ let playerChoice;
 // Randomly returns Rock, Paper, or Scissors
 function computerPlay() {
     output = Math.floor(Math.random() * 3); 
-    let decision;
-
-    switch (output) {
-        case 0:
-            decision = "Rock";
-            break;
-        case 1:
-            decision = "Paper";
-            break;
-        case 2:
-            decision = "Scissors";
-            break;
-        default:
-            return "Oops, something went wrong!";
-    }
+    let decision = choices[output];
 
     return decision;
 }
